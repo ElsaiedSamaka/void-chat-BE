6d0913f38d001c71e6a61a93bf50d2fe193e5684
Saied <elsaied.samaka1997@gmail.com>
2023-07-07 23:19:24 +0300

    commit 11

--- app/socketio/getMessages.js
+++ app/socketio/getMessages.js
@@ -1,12 +1,12 @@
 const Message = require("../models").message;
 const getMessages = async (socket, io, payload) => {
   console.log("getMessages called !!!!!!!!!!!");
-  // const { sender, recipient } = payload;
+  const { sender, recipient } = payload;
   const messages = await Message.findAll({
-    // where: {
-    //   senderId: sender,
-    //   recipientId: recipient,
-    // },
+    where: {
+      senderId: sender,
+      recipientId: recipient,
+    },
     include: {
       all: true,
     },
