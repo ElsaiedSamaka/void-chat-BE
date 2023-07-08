0dbff5e1df9292c4d037e47548bc1c6aef1663d2
Saied <elsaied.samaka1997@gmail.com>
2023-07-08 21:07:45 +0300

    commit 11

--- app/socketio/sendMessage.js
+++ app/socketio/sendMessage.js
@@ -14,17 +14,15 @@ const sendMessage = async (socket, io, data) => {
     include: { all: true },
   });
   // Create a unique room identifier for the conversation between the two users
-  const roomId = [data.sender, data.recipient].sort().join("-");
-  console.log("roomId", roomId);
+  // const roomId = [data.sender, data.recipient].sort().join("-");
+  // console.log("roomId", roomId);
+  // find the socketId of the user on recipientId and this will be our to()
+  const recipient = await User.findByPk(message.recipientId);
+  // find the socketId of the user on recipientId and this will be our to()
+  const sender = await User.findByPk(message.senderId);
   // Send the message to the appropriate room
-  io.to(roomId).emit("message", newMessage);
-  // // find the socketId of the user on recipientId and this will be our to()
-  // const recipient = await User.findByPk(message.recipientId);
-  // // find the socketId of the user on recipientId and this will be our to()
-  // const sender = await User.findByPk(message.senderId);
+  io.to([recipient.socketId, sender.socketId]).emit("newMessage", newMessage);
 
-  // io.to(recipient.socketId).emit("newMessage", newMessage);
-  // io.to(sender.socketId).emit("newMessage", newMessage);
   // in client we can handle whatever we want with newMessage event
 };
 
