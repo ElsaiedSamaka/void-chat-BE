95d04d157599295134fa2e44332dff43d9bc8127
Saied <elsaied.samaka1997@gmail.com>
2023-07-08 15:28:38 +0300

    commit 11

--- app/socketio/getMessages.js
+++ app/socketio/getMessages.js
@@ -1,21 +1,30 @@
 const { Op } = require("sequelize");
 
 const Message = require("../models").message;
+
 const getMessages = async (socket, io, payload) => {
-  console.log("getMessages called !!!!!!!!!!!");
   const { sender, recipient } = payload;
   console.log("sender", sender, "recipient", recipient);
+
   const messages = await Message.findAll({
     where: {
-      [Op.or]: {
-        senderId: sender,
-        recipientId: recipient,
-      },
+      [Op.or]: [
+        {
+          senderId: sender,
+          recipientId: recipient,
+        },
+        {
+          senderId: recipient,
+          recipientId: sender,
+        },
+      ],
     },
     include: {
       all: true,
     },
   });
+
   io.to(socket.id).emit("messages", messages);
 };
+
 module.exports = getMessages;
