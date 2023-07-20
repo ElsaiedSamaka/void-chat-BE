fb1e2b4fea092524f6204ac807350136ca829fb7
Saied <elsaied.samaka1997@gmail.com>
2023-07-20 15:31:22 +0300

    commit 11

--- app/socketio/sendMessage.js
+++ app/socketio/sendMessage.js
@@ -1,5 +1,3 @@
-const getConnectedUser = require("../helper/getConnectedUser");
-
 const Message = require("../models").message;
 const User = require("../models").user;
 
@@ -16,14 +14,11 @@ const sendMessage = async (socket, io, data) => {
   });
   // Create a room name based on the senderId and recipientId
   const roomName = `room:${sender.id}-${recipients[0].id}`;
-  // Send the message to each recipient
   for (const recipient of recipients) {
     await message.setRecipient(recipient)
   }
-
-  // Send the message to the sender as well
+  // Send the message to the room
   const newMessage = await Message.findByPk(message.id,{include:{all:true}})
-  const senderRoom = `user:${sender.id}`;
   io.to(roomName).emit('newMessage', newMessage);
 };
 
